e61339e71b07866b3e9c9de6e83d7ed474d895d3
Alasdair Henderson <alasdair.henderson1@lshtm.ac.uk>
2022-03-07 16:23:36 +0000

    finalise interaction pvals code and out

--- code/analysis/analysis_regressionInteractions.R
+++ code/analysis/analysis_regressionInteractions.R
@@ -1,7 +1,10 @@
+library(here)
 library(gtsummary)
+library(gt)
 library(survival)
 library(lmtest)
 library(broom)
+library(tidyverse)
 
 if (Sys.info()["user"] == "lsh1510922") {
   if (Sys.info()["sysname"] == "Darwin") {
@@ -17,7 +20,7 @@ dir.create(paste0(datapath, "out/supplementary/"))
 YY <- c("depression", "anxiety")
 XX <- c("psoriasis", "eczema")
 
-pval_interactions <- function(exposure, outcome, simple_model) {
+pval_interactions <- function(exposure, outcome) {
   ABBRVexp <- substr(exposure, 1, 3)
   
   df_model <-
@@ -62,16 +65,19 @@ pval_interactions <- function(exposure, outcome, simple_model) {
         "_mod7_interaction_calendar_modeldata.rds"
       )
     )
-  # tbl_regression(mod5, exp = T)
-  #
-  # mod5_tab <- mod5 %>%
-  #   tbl_regression(
-  #     exp = T,
-  #     include = c('exposed', 'exposed:agegroup', 'agegroup'),
-  #     conf.level = 0.99
-  #   )
   
   # lrtest ------------------------------------------------------------------
+  simple_model <-
+    readRDS(
+      paste0(
+        datapath,
+        "out/models_data/",
+        ABBRVexp,
+        "_",
+        outcome,
+        "_mod2_modeldata.rds"
+      )
+    )
   summary(simple_model, conf.int = F)
   summary(mod5, conf.int = F)
   summary(mod6, conf.int = F)
@@ -88,21 +94,18 @@ pval_interactions <- function(exposure, outcome, simple_model) {
                                    signif(lr2$`Pr(>Chisq)`[2], digits = 2),
                                    signif(lr3$`Pr(>Chisq)`[2], digits = 2)
                                  )) %>%
-    as.tibble()
+    as_tibble()
   names(inter_pval)[2] <-
     paste0(ABBRVexp, "_", substr(outcome, 1, 3))
   
   inter_pval %>%
     mutate_at(2, ~ prettyNum(.))
 }
-mod2 <-
-  coxph(Surv(t, out) ~ exposed + carstairs + cal_period + comorbid + cci + strata(setid),
-        data = df_model)
 
-int1 <- pval_interactions("eczema", "depression", simple_model = mod2)
-int2 <- pval_interactions("eczema", "anxiety", simple_model = mod2)
-int3 <- pval_interactions("psoriasis", "depression", simple_model = mod2)
-int4 <- pval_interactions("psoriasis", "anxiety", simple_model = mod2)
+int1 <- pval_interactions("eczema", "depression")
+int2 <- pval_interactions("eczema", "anxiety")
+int3 <- pval_interactions("psoriasis", "depression")
+int4 <- pval_interactions("psoriasis", "anxiety")
 
 int_table <- int2 %>% 
   left_join(int1, by = "interactions") %>% 
@@ -113,15 +116,18 @@ saveRDS(
   int_table,
   file = paste0(
     datapath,
-    "out/models_data/",
-    ABBRVexp,
-    "_",
-    outcome,
-    "_mod1_modeldata.rds"
+    "out/supplementary/",
+    "interaction_pvals.rds"
   )
 )
 
 int_table %>% 
+  mutate(ecz_anx = as.numeric(ecz_anx),
+         ecz_dep = as.numeric(ecz_dep),
+         pso_anx = as.numeric(pso_anx),
+         pso_dep = as.numeric(pso_dep),
+         ) %>% 
+  mutate_if(is.numeric, ~ifelse(.<0.0001, paste0("p<0.0001"), paste0(signif(., digits = 1)))) %>% 
   gt() %>% 
   cols_label(
     interactions = "",
@@ -130,11 +136,11 @@ int_table %>%
     pso_anx = md("**Anxiety**"),
     pso_dep = md("**Depression**"),
   ) %>% 
+  cols_align(columns = contains("ecz"),
+      align = "right") %>% 
+  cols_align(columns = contains("pso"),
+      align = "right") %>% 
   tab_spanner(label = md("**Eczema**"),
               columns = c(ecz_anx, ecz_dep)) %>% 
   tab_spanner(label = md("**Psoriasis**"),
               columns = c(pso_anx, pso_dep)) 
-mod5 %>% summary()
-
-ggforest2(simple_model, data = df_model)
-ggforest2(mod5, data = df_model)
