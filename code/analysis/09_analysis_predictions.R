58ca40b2ab00ca7fc7c1c8e8e72e1c33ab9def0e
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2022-04-22 13:22:40 +0100

    Update 09_analysis_predictions.R

--- code/analysis/09_analysis_predictions.R
+++ code/analysis/09_analysis_predictions.R
@@ -23,7 +23,7 @@ YY <- c("depression", "anxiety")
 XX <- c("psoriasis", "eczema")
 
 exposure <- XX[1]
-outcome <- YY[1]
+outcome <- YY[2]
 ABBRVexp <- substr(exposure, 1, 3)
 
 
@@ -40,7 +40,7 @@ sample_ids <- sample(unique(df_model$setid), size = 10000, replace = F)
 df_sample <- df_model %>% 
   filter(setid %in% sample_ids)
 
-smpl_fit <- coxph(Surv(t, out) ~ exposed + strata(setid), data = df_sample)
+smpl_fit_strat <- coxph(Surv(t, out) ~ exposed + strata(setid), data = df_sample)
 smpl_fit <- coxph(Surv(t, out) ~ exposed, data = df_sample)
 
 if (ABBRVexp == "ecz") {
@@ -59,31 +59,52 @@ if (ABBRVexp == "ecz") {
 table(df_sample$exposed)
 summary(cox_fit, conf.int = F)
 summary(smpl_fit, conf.int = F)
+summary(smpl_fit_strat, conf.int = F)
 
-fit <- survfit(smpl_fit, newdata = data.frame(exposed = c("Unexposed", "Psoriasis")))
-fitsum <- summary(fit)
-fitsum$year <- fitsum$time/365.25
 
-plot(fitsum$year, fitsum$surv[,1], type="l",ylim=c(0,1),col=c("black"),xlab="Year",ylab="Estimated survivor function")
-lines(fitsum$year, fitsum$surv[,2], type="l",ylim=c(0,1),col=c("grey"),xlab="Year",ylab="Estimated survivor function")
-legend(0,0.2,c("Unexposed",str_to_title(exposure)),col=c("black","grey"),lty=1,cex=1)
 
-# estimate baseline hazrd -------------------------------------------------
-vector_of_times <- df_sample$t[df_sample$out == 1]
-sort(vector_of_times)
+n_ids <- 100
+subset_sets <- sample_ids[1:n_ids]
+data_new_1 <- data.frame(exposed = c(rep("Psoriasis",n_ids)), setid = rep(subset_sets,1))
+data_new_0 <- data.frame(exposed = c(rep("Unexposed",n_ids)), setid = rep(subset_sets,1))
 
-predict_df <- data.frame(t = sort(vector_of_times))
+fit_strat_1 <- survfit(cox_fit, newdata = data_new_1)
+fit_strat_0 <- survfit(cox_fit, newdata = data_new_0)
+dim(fit_strat_1)
+dim(fit_strat_0)
+summary(fit_strat_0)
+fit_strat_0$strata
+strata_ids0 <- data.frame(nrows = fit_strat_0$strata, id = subset_sets, exposed = 0) %>% 
+  uncount(nrows)
+strata_ids1 <- data.frame(nrows = fit_strat_1$strata, id = subset_sets, exposed = 1) %>% 
+  uncount(nrows)
+df_predicted0 <- data.frame(t = fit_strat_0$time, 
+                           surv = fit_strat_0$surv, 
+                           surv_lo = fit_strat_0$lower, 
+                           surv_hi = fit_strat_0$upper)
+df_predicted1 <- data.frame(t = fit_strat_1$time, 
+                           surv = fit_strat_1$surv, 
+                           surv_lo = fit_strat_1$lower, 
+                           surv_hi = fit_strat_1$upper)
 
-predict_df <- predict_df %>% 
-  group_by(t) %>% 
-  summarise(dj = n())
 
-test <- predict_df %>% 
-  mutate()
+df_predicted_combo <- df_predicted0 %>% bind_cols(strata_ids0) %>% 
+  bind_rows(
+    bind_cols(df_predicted1, strata_ids1)
+  )
+df_predicted_combo$setid <- factor(df_predicted_combo$id)
+df_predicted_combo$exposed <- factor(df_predicted_combo$exposed)
+#df_sample %>% filter(setid == 22346748) %>% select(setid, patid, exposed, out, t)
 
-h0t <- function(
-  dj <- sum()
-)
+#plot(fit_strat)
+
+ggplot(df_predicted_combo, aes(x = t, y = surv, ymin = surv_lo, ymax = surv_hi, group = setid)) + 
+  geom_ribbon(aes(fill = exposed), alpha = 0.02) +
+  geom_line(aes(col = exposed), lwd = 0.5, alpha = 0.5) +
+  facet_wrap(~exposed)
+
+dev.copy(pdf, here("out/supplementary/survivor_curves_test.pdf"), width = 6, height = 6)
+dev.off()
 
 # specify the survival scenarios ------------------------------------------
 ## choose confounder covariates
