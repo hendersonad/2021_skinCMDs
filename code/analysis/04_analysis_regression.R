9f21b8b31aa5bb6a3a3ab20d58aaecc576873a83
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2022-05-13 12:32:50 +0100

    Remove building work from regression code file
    And add BMI2 + bmi_miss to the interaction models

--- code/analysis/04_analysis_regression.R
+++ code/analysis/04_analysis_regression.R
@@ -50,7 +50,6 @@ for (exposure in XX) {
 	
 	.dib(exposure)
 	
-	
 	if (samplingSmall) {
 		patids_anx <-  sample(unique(df_anx_split$setid), size = small_sample_size)
 		patids_dep <-  sample(unique(df_dep_split$setid), size = small_sample_size)
@@ -59,21 +58,6 @@ for (exposure in XX) {
 		df_dep_split <- df_dep_split %>% filter(setid %in% patids_dep)
 		export_plots <- F
 	}
-	
-	df_anx_split$t <-
-		as.numeric(df_anx_split$tstop - df_anx_split$tstart)
-	df_dep_split$t <-
-		as.numeric(df_dep_split$tstop - df_dep_split$tstart)
-	
-	df_anx_split$gc90days <-
-		factor(df_anx_split$gc90days, levels = c("not active", "active"))
-	df_dep_split$gc90days <-
-		factor(df_dep_split$gc90days, levels = c("not active", "active"))
-	
-	df_anx_split$bmi2 <-
-		df_anx_split$bmi - mean(df_anx_split$bmi, na.rm = T)
-	df_dep_split$bmi2 <-
-		df_dep_split$bmi - mean(df_dep_split$bmi, na.rm = T)
 	
 	# annual period prevalence plot -------------------------------------------
 	if (export_plots) {
@@ -287,17 +271,8 @@ for (exposure in XX) {
 		
 		rm(df_split_sum, df_split_cal, temp, p1, p2, p3, p4)
 		
-		# Run a simple Exponential regression --------------------------------
-		#.dib("Exp model")
-		#
-		#survreg(Surv(t) ~ exposed, dist = "exponential", data = df_anx_split) %>%
-		#	tbl_regression(exp = TRUE)
-		
 		# Run a simple KM analysis ------------------------------------------------
-		pdf(file = here::here("out", paste0("km_", ABBRVexp, "_anxiety_v2.pdf")),
-				6,
-				6,
-				onefile = F)
+		pdf(file = here::here("out", paste0("km_", ABBRVexp, "_anxiety_v2.pdf")),6,6,onefile = F)
 		km <- ggsurvplot(
 			fit = survminer::surv_fit(Surv(t / 365.25, out) ~ exposed, data = df_anx_split),
 			xlab = "Years",
@@ -312,12 +287,7 @@ for (exposure in XX) {
 		dev.off()
 		### improvements: add in a little tile with c(0,1) ylims to show true survival
 		
-		pdf(file = here::here("out", paste0(
-			"km_", ABBRVexp, "_depression_v2.pdf"
-		)),
-		6,
-		6,
-		onefile = F)
+		pdf(file = here::here("out", paste0("km_", ABBRVexp, "_depression_v2.pdf")),6,6,onefile = F)
 		km <- ggsurvplot(
 			fit = survminer::surv_fit(Surv(t / 365.25, out) ~ exposed, data = df_dep_split),
 			xlab = "Years",
@@ -343,88 +313,16 @@ for (exposure in XX) {
 			df_model <- df_dep_split
 		}
 
-		# Bit of variable formatting for output in regression tables --------------
-		df_model <- df_model %>% 
-			mutate(exposed = case_when(
-				exposed == 0 ~ "Unexposed",
-				exposed == 1 ~ stringr::str_to_title(paste0(exposure))
-			))
-			#rename(!!paste0(exposure) := exposed) %>% 
-		df_model$exposed <- factor(df_model$exposed, levels = c("Unexposed", str_to_title(exposure)))
-		var_label(df_model$exposed) <- "Exposure"
-		var_label(df_model$carstairs) <- "Carstairs index of deprivation"
-		levels(df_model$carstairs)[1] <- "1 (least deprived)"
-		levels(df_model$carstairs)[5] <- "5 (most deprived)"
-		var_label(df_model$cal_period) <- "Calendar Period"
-		var_label(df_model$cci) <- "Charlson's comorbidity index"
-		levels(df_model$cci) <- c("Low", "Moderate", "Severe")
-		var_label(df_model$agegroup) <- "Age group"
-		df_model$agegroup <- relevel(df_model$agegroup, ref = "50-59")
-		
-		# Mediators
-		var_label(df_model$bmi2) <- "BMI (centred)"
-		var_label(df_model$alc) <- "Alcohol misuse"
-		levels(df_model$alc) <- c("No", "Yes")
-		var_label(df_model$smokstatus) <- "Smoking status"
-		levels(df_model$smokstatus) <- stringr::str_to_title(levels(df_model$smokstatus))
-		var_label(df_model$gc90days) <- "Recent high dose glucocorticoid steroid use (<30days)"
-		levels(df_model$gc90days) <- c("No", "Yes")
-		df_model$sleep <- factor(df_model$sleep, levels = 0:1, labels = c("No", "Yes"))
-		var_label(df_model$sleep) <- "Sleep problems"
-		var_label(df_model$severity) <- paste(str_to_title(exposure), "severity", sep = " ")
-		levels(df_model$severity) <- str_to_title(levels(df_model$severity))
-		if (ABBRVexp == "ecz") {
-			df_model$comorbid <- df_model$asthma
-			var_label(df_model$comorbid) <- "Asthma"
-			levels(df_model$comorbid) <- c("No", "Yes")
-		} else{
-			df_model$comorbid <- df_model$arthritis
-			var_label(df_model$comorbid) <- "Arthritis"
-			levels(df_model$comorbid) <- c("No", "Yes")
-		}
-		
-		## impute smoking data 
-		df_model$smokstatus %>% table(useNA = "always")
-		df_model <- df_model %>% 
-		  group_by(setid, patid) %>% 
-		  mutate(smok_missing_always = ifelse(any(!is.na(smokstatus)), 0, 1),
-		         bmi_missing_always = ifelse(any(!is.na(bmi2)), 0, 1)
-		         )
-		levels(df_model$smokstatus) <- c(levels(df_model$smokstatus), "Missing")
-		
-		df_model$smokstatus[is.na(df_model$smokstatus) & df_model$smok_missing_always == 0] <- "Non-Smoker"
-		df_model$smokstatus %>% table(useNA = "always")
-		
-		df_model$bmi_miss <- 0
-		df_model$bmi_miss[is.na(df_model$bmi2)] <- 1
-		df_model$bmi_miss %>% table(useNA = "always")
-		df_model$bmi2[is.na(df_model$bmi2) & df_model$bmi_missing_always == 0] <- 0
-		
-		saveRDS(df_model, file = paste0(datapath, "out/models_data_impute/df_model", ABBRVexp, "_", outcome, ".rds"))
-		
 		# Run a simple Cox regression ----------------------------------------
 		.dib("Running model 1 (crude)")
 		mod1 <-
 			coxph(Surv(t, out) ~ exposed + strata(setid), data = df_model) #%>%
-		#mod1_tab <- mod1 %>% 
-		#	tbl_regression(exp = TRUE) %>%
-		#	add_n() %>% 
-		#	modify_footnote(update = estimate ~ "Adjusted for matched set (age, sex, GP)")
-		#mod1
 		
 		# Run a confounder Cox regression ----------------------------------------
 		.dib("Running model 2 (confounder)")
 		mod2 <-
 			coxph(Surv(t, out) ~ exposed + carstairs + cal_period + comorbid + cci + strata(setid),
 						data = df_model)
-		# mod2_exp <- mod2 %>% tbl_regression(exp = T,
-		# 																		include = "exposed",
-		# 																		conf.level = 0.99) %>% 
-		# 	add_n() %>% 
-		# 	modify_footnote(update = estimate ~ "Additionally adjusted for calendar period and comorbidities")
-		# mod2_full <- mod2 %>% tbl_regression(exp = T, conf.level = 0.99) %>% 
-		# 	modify_footnote(update = estimate ~ "Additionally adjusted for calendar period and comorbidities")
-		#mod2
 		
 		# Run a mediator Cox regression -------------------------------------------
 		.dib("Running model 3 (mediator)")
@@ -440,73 +338,6 @@ for (exposure in XX) {
 					Surv(t, out) ~ exposed + carstairs + cal_period + comorbid + cci + bmi2 + bmi_miss + alc + smokstatus + strata(setid),
 					data = df_model
 				) 
-		}
-		#mod3
-		# mod3_exp <- mod3 %>% tbl_regression(exp = T,
-		# 																		include = "exposed",
-		# 																		conf.level = 0.99) %>% 
-		# 	add_n() 
-		# mod3_full <- mod3 %>% tbl_regression(exp = T, conf.level = 0.99) %>% 
-		# 	modify_footnote(update = estimate ~ "Additionally adjusted for calendar period and comorbidities")
-		# if(exposure == "eczema"){
-		# 	mod3_exp <- mod3_exp %>%
-		# 		modify_footnote(update = estimate ~ "Additionally adjusted for BMI, alcohol misuse, smoking status, sleep problems, oral glucocorticoid use")
-		# 	mod3_full <- mod3_full %>% 
-		# 		modify_footnote(update = estimate ~ "Additionally adjusted for BMI, alcohol misuse, smoking status, sleep problems, oral glucocorticoid use")
-		# }else if (exposure == "psoriasis"){
-		# 	mod3_exp <- mod3_exp %>%
-		# 		modify_footnote(update = estimate ~ "Additionally adjusted for BMI, alcohol misuse, smoking status")
-		# 	mod3_full <- mod3_full %>% 
-		# 		modify_footnote(update = estimate ~ "Additionally adjusted for BMI, alcohol misuse, smoking status")
-		# }
-		
-		# summarise results -------------------------------------------------------
-		# calculate the total N by exposed
-		# tbl_n <- df_model %>% select(exposed) %>%
-		# 	tbl_summary(statistic = everything() ~ "{n}") %>%
-		# 	modify_header(stat_0 ~ "**N**") %>%
-		# 	modify_footnote(everything() ~ NA)
-		# 
-		# # calculate the event N by exposed
-		# tbl_nevent <- df_model %>% filter(out == 1) %>% select(exposed) %>%
-		# 	tbl_summary(statistic = everything() ~ "{n}") %>%
-		# 	modify_header(stat_0 ~ "**Event N**")  %>%
-		# 	modify_footnote(everything() ~ NA)
-		# 
-		# # calculate the pyars N by exposed
-		# tbl_pyars <- df_model %>% select(exposed, t) %>%
-		# 	group_by(exposed) %>%
-		# 	summarise(pyars = round(sum(t))) %>%
-		# 	uncount(pyars) %>%
-		# 	tbl_summary(statistic = everything() ~ "{n}") %>% 
-		# 	modify_header(stat_0 ~ "**P-years**")  %>%
-		# 	modify_footnote(everything() ~ NA)
-		# 
-		# merge tbls together
-		# tbls <-
-		# 	tbl_merge(
-		# 		list(tbl_n, tbl_nevent, tbl_pyars, mod1_tab, mod2_exp, mod3_exp),
-		# 		tab_spanner = c(NA, NA, NA, "**Crude**", "**Confounder**", "**Mediator**")
-		# 	) 
-		# 
-		# tbls_full <- tbl_merge(
-		# 	tbls = list(tbl_n, tbl_nevent, tbl_pyars, mod1_tab, mod2_full, mod3_full),
-		# 	tab_spanner = c(NA, NA, NA, "**Crude**", "**Confounder**", "**Mediator**")
-		# )
-		
-		if (export_tables) {
-			tbls %>%
-				gtsummary::as_gt() %>%
-				gt::gtsave(
-					filename =  paste0("tabls1A_", ABBRVexp, "_", outcome, ".html"),
-					path = here::here("out/analysis")
-				)
-			tbls_full %>%
-				gtsummary::as_gt() %>%
-				gt::gtsave(
-					filename =  paste0("tabls1B_", ABBRVexp, "_", outcome, "_full.html"),
-					path = here::here("out/analysis")
-				)
 		}
 		
 		if (export_models) {
@@ -552,22 +383,16 @@ for (exposure in XX) {
 		if (ABBRVexp == "ecz") {
 			mod4 <-
 				coxph(
-					Surv(t, out) ~ severity + carstairs + cal_period + comorbid + cci + bmi2 + sleep + alc + smokstatus + gc90days + strata(setid),
+					Surv(t, out) ~ severity + carstairs + cal_period + comorbid + cci + bmi2 + bmi_miss + sleep + alc + smokstatus + gc90days + strata(setid),
 					data = df_model
 				) 
 		} else if (ABBRVexp == "pso") {
 			mod4 <-
 				coxph(
-					Surv(t, out) ~ severity + carstairs + cal_period + comorbid + cci + bmi2 + alc + smokstatus + strata(setid),
+					Surv(t, out) ~ severity + carstairs + cal_period + comorbid + cci + bmi2 + bmi_miss + alc + smokstatus + strata(setid),
 					data = df_model
 				) 
 		}
-		# mod4_tab <- mod4 %>%
-		# 	tbl_regression(exp = T,
-		# 								 include = "severity",
-		# 								 conf.level = 0.99) %>%
-		# 	add_n() %>%
-		# 	add_nevent()
 		
 		# Interaction -------------------------------------------------------------
 		.dib("Running model 5 (interaction age)")
@@ -584,11 +409,6 @@ for (exposure in XX) {
 					data = df_model
 				) 
 		}
-		# mod5_tab <- mod5 %>%
-		# 	tbl_regression(exp = T,
-		# 								 include = c(exposed, exposed:agegroup, agegroup),
-		# 								 conf.level = 0.99) %>%
-		# 	add_glance_source_note(include = n) 
 		
 		.dib("Running model 6 (interaction comorbid)")
 		if (ABBRVexp == "ecz") {
@@ -604,10 +424,6 @@ for (exposure in XX) {
 					data = df_model
 				) 
 		}
-		# mod6_tab <- mod6 %>%
-		# 	tbl_regression(exp = T,
-		# 								 include = c(exposed, exposed:comorbid, comorbid),
-		# 								 conf.level = 0.99) 
 		
 		.dib("Running model 7 (interaction calendar)")
 		if (ABBRVexp == "ecz") {
@@ -623,10 +439,7 @@ for (exposure in XX) {
 					data = df_model
 				)
 		}
-		# mod7_tab <- mod7 %>%
-		# 	tbl_regression(exp = T,
-		# 								 include = c(exposed, exposed:cal_period, cal_period),
-		# 								 conf.level = 0.99) 
+		
 		.dib("Running model 8 (interaction carstairs)")
 			mod8 <-
 				coxph(
