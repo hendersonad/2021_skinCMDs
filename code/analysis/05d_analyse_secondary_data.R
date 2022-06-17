33a2bc62cacad4a45381d7249b638a0b70553727
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2022-06-17 12:28:59 +0100

    Update 05d_analyse_secondary_data.R

--- code/analysis/05d_analyse_secondary_data.R
+++ code/analysis/05d_analyse_secondary_data.R
@@ -54,6 +54,17 @@ for(exposure in XX){
 	        "_mod4_severity_modeldata.rds"
 	      )
 	    )
+	  mod4b <-
+	    readRDS(
+	      paste0(
+	        datapath,
+	        "out/models_data/",
+	        ABBRVexp,
+	        "_",
+	        outcome,
+	        "_mod10_severity_confounded_modeldata.rds"
+	      )
+	    )
 	  mod5 <-
 	    readRDS(
 	      paste0(
@@ -87,11 +98,26 @@ for(exposure in XX){
 	        "_mod7_interaction_calendar_modeldata.rds"
 	      )
 	    )
-	  
+	  mod8 <-
+	    readRDS(
+	      paste0(
+	        datapath,
+	        "out/models_data/",
+	        ABBRVexp,
+	        "_",
+	        outcome,
+	        "_mod8_interaction_carstairs_modeldata.rds"
+	      )
+	    )
+
 	  out <- substr(outcome, 1, 3)
 	  df_model <- get(paste0("df_model_", out))
 	  
 	  mod4_tab <- mod4 %>%
+	    tbl_regression(exp = T,
+	                   #include = "severity",
+	                   conf.level = 0.95)
+	  mod4_confounder_tab <- mod4b %>%
 	    tbl_regression(exp = T,
 	                   #include = "severity",
 	                   conf.level = 0.95)
@@ -101,15 +127,15 @@ for(exposure in XX){
 	      exp = T,
 	      include = c('exposed', 'exposed:agegroup', 'agegroup'),
 	      conf.level = 0.95
-	    ) 
-	  
+	    )
+
 	  mod6_tab <- mod6 %>%
 	    tbl_regression(
 	      exp = T,
 	      include = c('exposed', 'exposed:comorbid', 'comorbid'),
 	      conf.level = 0.95
 	    )
-	  
+
 	  mod7_tab <- mod7 %>%
 	    tbl_regression(
 	      exp = T,
@@ -117,26 +143,36 @@ for(exposure in XX){
 	      conf.level = 0.95
 	    )
 	  
+	  mod8_tab <- mod8 %>%
+	    tbl_regression(
+	      exp = T,
+	      include = c('exposed', 'exposed:carstairs', 'carstairs'),
+	      conf.level = 0.95
+	    )
+
 	  # summarise results -------------------------------------------------------
-	  tbls_secondary <- tbl_merge(
-	    tbls = list(mod5_tab, mod6_tab, mod7_tab),
-	    tab_spanner = c("**Age group**",
-	                    "**Comorbidity**",
-	                    "**Calendar Period**")
+	  tbls_severity <- tbl_merge(
+	    tbls = list(mod4_confounder_tab, mod4_tab),
+	    tab_spanner = c("**Confounder adjusted**",
+	                    "**Mediator adjusted**")
 	  )
-	  
 	    mod4_tab %>%
 	      gtsummary::as_gt() %>%
 	      gt::gtsave(
 	        filename =  paste0("tab9_", ABBRVexp, "_", outcome, "_severity.html"),
 	        path = here::here("out/tables")
 	      )
-	    tbls_secondary %>%
-	      gtsummary::as_gt() %>%
-	      gt::gtsave(
-	        filename =  paste0("tab10_", ABBRVexp, "_", outcome, "_interaction.html"),
-	        path = here::here("out/tables")
-	      )
+	  
+	 tbls_secondary <- tbl_merge(
+	    tbls = list(mod5_tab, mod6_tab, mod7_tab, mod8_tab),
+	    tab_spanner = c("**Age group**",
+	                    "**Comorbidity**",
+	                    "**Calendar Period**",
+	                    "**Deprivation**")
+	  )
+	 tbls_secondary %>%
+	   gtsummary::as_gt() %>% 
+	   gt::gtsave(filename =  paste0("tab10_", ABBRVexp, "_", outcome, "_interaction.html"),path = here::here("out/tables"))
 	}
 	rm(mod4, mod5, mod6, mod7,
 	   mod4_tab, mod5_tab, mod6_tab, mod7_tab)
