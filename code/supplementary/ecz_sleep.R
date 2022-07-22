13eaea4ed11d20e1da536b3751295a974d115d52
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2022-07-22 17:45:44 +0100

    Update ecz_sleep.R

--- code/supplementary/ecz_sleep.R
+++ code/supplementary/ecz_sleep.R
@@ -28,8 +28,6 @@ ABBRVexp <- str_sub(exposure,1 ,3)
 
 .dib(exposure)
 
-df_anx_split <- readRDS(paste0(datapath, "out/df_model", ABBRVexp, "_anxiety.rds"))
-df_dep_split <- readRDS(paste0(datapath, "out/df_model", ABBRVexp, "_depression.rds"))
 cohort <- haven::read_dta(paste0(datapath, "out/getmatchedcohort-", exposure, "-main-mhealth.dta"))
 severity <- haven::read_dta(paste0(datapath, "out/variables-ecz-severity.dta"))
 
@@ -67,8 +65,13 @@ table(cohort_severity$severity)
 
 cohort_sleep <- cohort_severity %>% 
   left_join(severity, by = "patid") %>% 
-  dplyr::select(setid, patid, exposed, indexdate, severity) %>% 
+  dplyr::select(setid, patid, exposed, indexdate, enddate, severity) %>% 
   left_join(ecz_sleep_unique, by = "patid")
+
+## censor events that are _after_ the enddate for that patid
+cohort_sleep$censorevent <- as.numeric(cohort_sleep$eventdate>cohort_sleep$enddate)
+cohort_sleep$censorevent[is.na(cohort_sleep$censorevent)] <- 0
+cohort_sleep$eventdate[cohort_sleep$censorevent == 1] <- NA
 
 # run 2x2 tables ----------------------------------------------------------
 twoXtwo <- function(df, exp, out){
@@ -93,7 +96,6 @@ df_out <- cohort_sleep
 df_out$src[is.na(df_out$src)] <- 0
 df_out$poss[is.na(df_out$poss)] <- 0
 
-# all sleep codes - drugs only --------------------------------------------
 df_tab <- df_out %>% 
   ungroup() %>% 
   mutate(readcode = ifelse(src == 1, 1, 0)) %>% 
@@ -107,25 +109,44 @@ df_tab <- df_out %>%
 table(df_tab$severity)
 table(cohort_severity$severity)
 
+df_tab$sleep_def <- df_tab$readcode + df_tab$drug_def
+df_tab[df_tab$sleep_def == 2] <- 1
+
+# all sleep codes - drugs only --------------------------------------------
 tab_sleep_readcode <- twoXtwo(df = df_tab, exp = "severity", out = "readcode") %>% drop_na() %>% mutate(sleep_class = "read")
 tab_sleep_drugs_all <- twoXtwo(df = df_tab, exp = "severity", out = "drug_poss") %>% drop_na() %>% mutate(sleep_class = "drugs_poss")
 tab_sleep_drugs_def <- twoXtwo(df = df_tab, exp = "severity", out = "drug_def") %>% drop_na() %>% mutate(sleep_class = "drugs_def")
+tab_sleep <- twoXtwo(df = df_tab, exp = "severity", out = "sleep_def") %>% drop_na() %>% mutate(sleep_class = "sleep_def")
+
+ecz_sleep_readcode <- twoXtwo(df = df_tab, exp = "exposed", out = "readcode") %>% drop_na() %>% mutate(sleep_class = "read")
+ecz_sleep_drugs_all <- twoXtwo(df = df_tab, exp = "exposed", out = "drug_poss") %>% drop_na() %>% mutate(sleep_class = "drugs_poss")
+ecz_sleep_drugs_def <- twoXtwo(df = df_tab, exp = "exposed", out = "drug_def") %>% drop_na() %>% mutate(sleep_class = "drugs_def")
+ecz_sleep <- twoXtwo(df = df_tab, exp = "exposed", out = "sleep_def") %>% drop_na() %>% mutate(sleep_class = "sleep_def")
 
 tab_sleep_out <- bind_rows(tab_sleep_readcode,
                            tab_sleep_drugs_def, 
-                           tab_sleep_drugs_all)
+                           tab_sleep_drugs_all,
+                           tab_sleep,
+                           ecz_sleep_readcode,
+                           ecz_sleep_drugs_all,
+                           ecz_sleep_drugs_def,
+                           ecz_sleep) %>% 
+  filter(val != "0")
+tab_sleep_out$val[tab_sleep_out$val=="1"] <- "Eczema (any severity)"
 
 gt_sleep <- tab_sleep_out %>%
 	dplyr::select(-exposure,-Miss, -sleep_class) %>%
   mutate(val = str_to_title(val)) %>% 
 	gt::gt() %>%
-	tab_row_group(label = "All sleep Read codes",
-								rows = 1:4) %>%
-	tab_row_group(label = "Definite sleep drugs",
-								rows = 5:8) %>%
-	tab_row_group(label = "All sleep drugs",
-								rows = 9:12) %>%
-	gt::tab_header(title = "Sleep problems by eczema severity") %>%
+  tab_row_group(label = "Definite sleep drugs + Read codes",
+                rows = c(20, 13:16)) %>%
+  tab_row_group(label = "All sleep Read codes",
+                rows = c(17, 1:4)) %>%
+  tab_row_group(label = "Definite sleep drugs",
+                rows = c(19, 5:8)) %>%
+  tab_row_group(label = "All sleep drugs",
+                rows = c(18, 9:12)) %>%
+  gt::tab_header(title = "Sleep problems by eczema severity") %>%
 	gt::fmt_number(columns = c(3, 5), decimals = 1) %>%
 	gt::fmt_number(columns = c(2, 4), decimals = 0) %>%
 	gt::data_color(
@@ -155,11 +176,11 @@ gt_sleep %>%
 p3 <- tab_sleep_out %>% 
 	  mutate(lab = paste0(prettyNum(Yes_pc, big.mark = ",", digits = 3), "%")) %>% 
 	  mutate(val = str_to_title(val)) %>% 
-	  mutate(val = factor(val, levels = c("None", "Mild", "Moderate", "Severe"))) %>% 
+	  mutate(val = factor(val, levels = c("None", "Mild", "Moderate", "Severe", "Eczema (Any Severity)"))) %>% 
 	  mutate(sleep_class = 
 	           factor(sleep_class, 
-	                  levels = c("read", "drugs_poss", "drugs_def"), 
-	                  labels = c("All sleep Read codes", "All sleep drugs", "Definite sleep drugs"))) %>% 
+	                  levels = c("read", "drugs_poss", "drugs_def", "sleep_def"), 
+	                  labels = c("All sleep Read codes", "All sleep drugs", "Definite sleep drugs", "Definite sleep drugs + Read codes"))) %>% 
 	ggplot(aes(x = val, y = sleep_class, size = Yes, fill = Yes_pc)) +
 	  geom_point(alpha = 0.6, shape = 21, color = 1) +
 	  geom_text(aes(label = lab), nudge_y = 0, size = 4, col = 1) +
@@ -309,5 +330,78 @@ pdf(here::here("out/supplementary/sleep_codes_exp.pdf"), width = 7, height = 10)
   plot_grid(p3, p2, ncol = 1 , rel_heights = c(1,2), labels = "AUTO")
 dev.off()
 
+# Eczema yes/no results ---------------------------------------------------
+tab_sleep_readcode <- twoXtwo(df = df_tab, exp = "exposed", out = "readcode") %>% drop_na() %>% mutate(sleep_class = "read")
+tab_sleep_drugs_all <- twoXtwo(df = df_tab, exp = "exposed", out = "drug_poss") %>% drop_na() %>% mutate(sleep_class = "drugs_poss")
+tab_sleep_drugs_def <- twoXtwo(df = df_tab, exp = "exposed", out = "drug_def") %>% drop_na() %>% mutate(sleep_class = "drugs_def")
+tab_sleep <- twoXtwo(df = df_tab, exp = "exposed", out = "sleep_def") %>% drop_na() %>% mutate(sleep_class = "sleep_def")
+
+tab_sleep_out <- bind_rows(tab_sleep_readcode,
+                           tab_sleep_drugs_def, 
+                           tab_sleep_drugs_all,
+                           tab_sleep)
+
+gt_sleep <- tab_sleep_out %>%
+  dplyr::select(-exposure,-Miss, -sleep_class) %>%
+  mutate(val = str_to_title(val)) %>% 
+  gt::gt() %>%
+  tab_row_group(label = "Definite sleep drugs + Read codes",
+                rows = 7:8) %>%
+  tab_row_group(label = "All sleep Read codes",
+                rows = 1:2) %>%
+  tab_row_group(label = "Definite sleep drugs",
+                rows = 3:4) %>%
+  tab_row_group(label = "All sleep drugs",
+                rows = 5:6) %>%
+  gt::tab_header(title = "Sleep problems by eczema diagnosis") %>%
+  gt::fmt_number(columns = c(3, 5), decimals = 1) %>%
+  gt::fmt_number(columns = c(2, 4), decimals = 0) %>%
+  gt::data_color(
+    columns = c(Yes_pc),
+    colors = scales::col_numeric(
+      palette = paletteer::paletteer_c(palette = "viridis::inferno",
+                                       n = 100) %>% as.character(),
+      domain = c(
+        min(tab_sleep_out$Yes_pc, na.rm = T),
+        max(tab_sleep_out$Yes_pc, na.rm = T)
+      )
+    )
+  ) %>%
+  tab_footnote(footnote = md(paste0("*n* = ", prettyNum(dim(df_tab)[1], big.mark = ","))),
+               locations = cells_title("title")) %>% 
+  cols_label(val = "Eczema",
+             No_pc = "%",
+             Yes_pc = "%") 
+
+gt_sleep %>% 
+  gt::gtsave(
+    filename =  paste0("eczema_sleep_cohort_yesno.html"),
+    path = here::here("out/supplementary")
+  )
+
+# bubble plot for proportion with code ------------------------------------
+p3 <- tab_sleep_out %>% 
+  mutate(lab = paste0(prettyNum(Yes_pc, big.mark = ",", digits = 3), "%")) %>% 
+  mutate(val = factor(val, levels = 0:1, labels = c("Unexposed", "Eczema"))) %>% 
+  mutate(sleep_class = 
+           factor(sleep_class, 
+                  levels = c("read", "drugs_poss", "drugs_def", "sleep_def"), 
+                  labels = c("All sleep Read codes", "All sleep drugs", "Definite sleep drugs", "Definite sleep drugs + Read codes"))) %>% 
+  ggplot(aes(x = val, y = sleep_class, size = Yes, fill = Yes_pc)) +
+  geom_point(alpha = 0.6, shape = 21, color = 1) +
+  geom_text(aes(label = lab), nudge_y = 0, size = 4, col = 1) +
+  scale_size(range = c(5, 20), name = "*n* with code") +
+  scale_fill_viridis_c(option = "inferno", name = "*%* with code") +
+  labs(x = "Eczema severity", y = "") +
+  guides(size = "none") +
+  scale_y_discrete(limits=rev) +
+  theme_ali() +
+  theme(legend.position = "top", 
+        legend.title = element_markdown(),
+        axis.line = element_line(size = rel(0.5)),
+        panel.grid.major.y = element_blank(),
+        panel.grid = element_blank())
+p3
+dev.copy(pdf, here::here("out/supplementary/sleep_data_eczema_yesno.pdf"), width = 8, height = 6); dev.off()
 
 
